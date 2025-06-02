2c159dcafbd1493fe138b7684eae6471591cfad9
Noah <iriaroan@naver.com>
2025-06-02 12:08:45 +0900

    init

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: '/KOR-Ryat',
+  assetPrefix: '/kash-client/',
+
+  trailingSlash: true,
+};
+
+export default nextConfig;
